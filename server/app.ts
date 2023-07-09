79f1265041085b3bcfa981fa7fdfce4fb499be92
Amirali Lotfi <liamirali.lotfi@gmail.com>
2023-07-09 23:01:49 +0330

    Add MongoDB

--- server/app.ts
+++ server/app.ts
@@ -3,6 +3,7 @@ import authRoutes from "./routes/auth";
 import path from "path";
 import bodyParser from "body-parser";
 import { rootDir } from "./utils/path";
+import { dbConnect } from "./utils/database";
 
 const app = express();
 const port = 5000;
@@ -15,5 +16,7 @@ app.use("/", (req: Request, res: Response) => {
   console.log("Hello!", rootDir);
   res.sendFile(path.join(rootDir, "view", "index.html"));
 });
+
+dbConnect().catch(console.dir);
 
 app.listen(port);
