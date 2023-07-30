52ad7918dc233a6f81c751fb332482d9a65c38fb
Amirali Lotfi <liamirali.lotfi@gmail.com>
2023-07-30 16:41:41 +0330

    Expand name property to first name and last name

--- models/User.ts
+++ models/User.ts
@@ -1,13 +1,15 @@
-import mongoose, { Model, Schema } from "mongoose";
+import mongoose, { Model } from "mongoose";
 
 interface IUser extends Document {
-  name: string;
+  firstName: string;
+  lastName: string;
   email: string;
   password: string;
 }
 
 const userSchema = new mongoose.Schema<IUser>({
-  name: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
   // expenses: [Expense],
