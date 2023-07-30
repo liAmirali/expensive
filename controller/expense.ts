c2deccbe709f6755b517f03f5832de8c23bfeb9a
Amirali Lotfi <liamirali.lotfi@gmail.com>
2023-07-30 21:40:31 +0330

    Add expenses to the user who created it

--- controller/expense.ts
+++ controller/expense.ts
@@ -1,17 +1,40 @@
 import { Request, Response } from "express";
 import { Expense } from "../models/Expense";
 import { ObjectId } from "mongodb";
+import { User } from "../models/User";
+import { ApiError } from "../utils/errors";
 
-export const addExpense = (req: Request, res: Response) => {
+export const addExpense = async (req: Request, res: Response) => {
   const { value, category, description, currency } = req.body;
+
+  const userId = req.user!.userId; // We authenticate user in a middleware so the user object definitely exists
 
   const newExpense = new Expense({
     value,
     category,
     description,
     currency,
-    created_by: new ObjectId("00000020"),
+    created_by: new ObjectId(userId),
   });
+
+  if (!userId) {
+    const error = new ApiError("User not found.");
+    error.statusCode = 401;
+    throw error;
+  }
+  const found = await User.findOneAndUpdate(
+    { _id: new ObjectId(userId) },
+    {
+      $push: {
+        expenses: newExpense,
+      },
+    }
+  );
+  if (found === null) {
+    const error = new ApiError("Couldn't update user.");
+    error.statusCode = 500;
+    throw error;
+  }
 
   newExpense.save();
 
