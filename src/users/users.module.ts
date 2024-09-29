b5183a85ddbceaac1a3a88a6b232aea67a11ee12
Amirali Lotfi <liamirali.lotfi@gmail.com>
2024-09-29 21:35:50 +0330

    Add auth guard to the whole app and add Public decorator

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from 'src/auth/auth.gaurd';
 
 @Module({
-  providers: [UsersService],
+  providers: [
+    UsersService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
   exports: [UsersService],
 })
 export class UsersModule {}
