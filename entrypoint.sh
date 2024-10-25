9d468ad62bb44d9e0074513ad3c2c6327fece510
Amirali Lotfi <liamirali.lotfi@gmail.com>
2024-10-25 16:30:22 +0330

    Add prisma generate to entry point

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+echo "### Running the entryoint script ###"
 
 chmod +x "./wait-for-it.sh"
 bash "./wait-for-it.sh" postgres:5432
 
 npx prisma migrate deploy
+
+npx prisma generate
 
 npm run start:dev
